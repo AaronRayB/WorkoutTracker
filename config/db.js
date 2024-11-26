ce9cd9c59829431d14538ddbf4ece01085bfb2c6
AaronRayB <aaron.brittannia@ontariotechu.net>
2024-11-26 11:00:59 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Use your MongoDB URI directly here (for testing purposes only)
-    await mongoose.connect('mongodb+srv://aaronbrittanniaray:Babyray18@cluster0.f816t.mongodb.net/workoutTracker?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://aaronbrittanniaray:Babyray18@cluster0.f816t.mongodb.net/workoutTracker?retryWrites=true&w=majority');
     console.log('MongoDB connected...');
   } catch (error) {
     console.error('Database connection failed:', error);
